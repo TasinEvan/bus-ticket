e7305d64bdde9e896eaff057b98c1b8c03ead6bd
Tasin Evan <tasinmosleh508@gmail.com>
2024-02-19 04:20:41 +0600

    semi final aDDED

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,12 @@
 
 // utility
+function enableApply (){
+  if( selectedSeatCount === 4){
+   const couponApply = document.getElementById('apply-btn-ID')
+   couponApply.disabled = false
 
+  }
+ }
 
 
 
@@ -12,6 +18,7 @@ function forScrollToSection(){
 
 
 }
+let selectedSeatCount = 0
 
 let seatCount = 40
 let bookingCount = 0
@@ -19,17 +26,85 @@ const seatContainer = document.getElementById('seatContainer-ID').addEventListen
     const seats = this.querySelectorAll('.btn')
     
   for( let seat of seats ){
-   if(seat === event.target) {
+    if( selectedSeatCount >= 4){
+      alert('You can not select more than 4 seat')
+      return
+    }
+  
+    if(seat === event.target && !seat.classList.contains('selected')) {
+      seat.classList.add('selected'); 
     seat.style.backgroundColor ='#1DD100'
     bookingCount = bookingCount + 1
     seatCount = seatCount - 1
+    selectedSeatCount = selectedSeatCount + 1
     getInnerTextbyId('seat-booking', bookingCount)
     getInnerTextbyId('seatLeft-ID', seatCount)
+
+     
+  
+
+
+    // table 
+     const tableContainer = document.getElementById('table-ID')
+      const row = document.createElement('tr')
+      const seatNum = document.createElement('td')
+     seatNum.textContent = event.target.innerText
+     row.appendChild(seatNum)
+     const seatClass = document.createElement('td')
+     seatClass.textContent = 'Economy'
+     row.appendChild(seatClass)
+     const seatPrice = document.createElement('td')
+     seatPrice.textContent = '550'
+     row.appendChild(seatPrice)
+     tableContainer.appendChild(row)
+
+    //  costings
+    let totalCost = 0
+    let priceOfTicket = 550
+    priceOfTicket = priceOfTicket * selectedSeatCount
+     totalCost = totalCost + priceOfTicket
+    getInnerTextbyId('total-cost', totalCost)
+    getInnerTextbyId('grand-cost', totalCost)
+      
+
+
+
+    enableApply ()
+
+    document.getElementById('apply-btn-ID').addEventListener('click',function(){
+      const inputText = document.getElementById('input-Id').value
+      const couponElement = document.getElementById('couponBox')
+      
+    if( inputText === 'NEW15' ){
+       
+       getInnerTextbyId('grand-cost', totalCost * 0.85 )
+       couponElement.classList.add('hidden')
+      }
+      else if (inputText === 'couple 20') {
+        getInnerTextbyId('grand-cost', totalCost * 0.80 )
+        couponElement.classList.add('hidden')
+      }
+      else{getInnerTextbyId('grand-cost', totalCost )
+        alert('Invalid Coupon code')
+         return
+      }
     
+    })
+        
 
 
+
+
+
+
+     return
+  
    }
 
-  }
+
+  }  
+  
 })
 
+
+ 
