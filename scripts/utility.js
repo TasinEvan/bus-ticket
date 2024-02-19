7b8fb881cb08766ab706b84da67ebc818598e02d
Tasin Evan <tasinmosleh508@gmail.com>
2024-02-19 08:06:32 +0600

    footer addedddddd yesss

--- scripts/utility.js
+++
@@ -1,8 +0,0 @@
-// utility
-function getInnerTextbyId(elementID , value) {
-    let element = document.getElementById(elementID);
-    
-      document.getElementById(elementID).innerText = value
-    
-       
-    } 
